06ff00647400574c4ce1613574d0ff655b86ef09
Megumi Ando <remiria@flame-daybreak.net>
2023-04-19 23:56:22 +0900

    構造調整する

---
+++ docs/index.md
@@ -0,0 +1,14 @@
+---
+slug: /
+---
+
+# Index
+
+これは Index です。
+
+```python
+import os
+
+if os.path.exists():
+    pritn('hogehoge')
+```
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,9 +6,13 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'Reincarnation+#Tech',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
+  
+  projectName: 'fereria.github.io',
+  organizationName: 'reincarnation_tech_dev',
+  trailingSlash: false,
 
   // Set the production url of your site here
   url: 'https://fereria.github.io',
@@ -38,6 +42,7 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+          routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
@@ -48,8 +53,6 @@ const config = {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -64,7 +67,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Reincarnation+#Tech りにゅーある中',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
