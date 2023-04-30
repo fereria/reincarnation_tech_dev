59323f416a1e9ef6130d05e60a00b1296d6e19d5
Megumi Ando <remiria@flame-daybreak.net>
2023-04-30 20:06:08 +0900

    AssetResolutionを追加

--- src/components/sample.js
+++ src/components/sample.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-export default function HelloWorld({url}) {
+const LinkCard = ({ title, description, link }) => {
   return (
-    <span>
-        Hello World!!!
-    </span>
+    <div className="link-card">
+      <h2>{title}</h2>
+      <p>{description}</p>
+      <Link to={link}>Go to {title}</Link>
+    </div>
   );
-}
+};
+
+LinkCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired,
+};
+
+export default LinkCard;
--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import MDXComponents from '@theme-original/MDXComponents';
-import HelloWorld from '@site/src/components/sample.js';
+import LinkCard from '@site/src/components/sample.js';
 import { TwitterTweetEmbed } from "react-twitter-embed";
 import Gist from "react-gist";
 
 export default {
   // Re-use the default mapping
   ...MDXComponents,
-  HelloWorld,
+  LinkCard,
   Gist,
   TwitterTweetEmbed
 };
