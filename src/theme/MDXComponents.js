e43b5451423f92b7c4331a12fb80bd720cdb7f0b
Megumi Ando <remiria@flame-daybreak.net>
2023-04-30 15:21:16 +0900

    MDXComponentを試す

---
+++ src/theme/MDXComponents.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import MDXComponents from '@theme-original/MDXComponents';
+import HelloWorld from '@site/src/components/sample.js';
+
+export default {
+  // Re-use the default mapping
+  ...MDXComponents,
+  HelloWorld
+};
